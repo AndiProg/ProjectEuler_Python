13728e5c1d31642f95f1ff565d3e027507f9ab06
Andi <Andi@LAPTOP>
2018-04-14 16:43:43 +0200

    0022 added

--- ProjectEuler_Python/0021.py
+++ ProjectEuler_Python/0021.py
@@ -1,1 +1,21 @@
-print(1+1)
+def d(n):
+    divisorSum = 0
+    for divisor in range(1,n):
+        if n % divisor == 0:
+            divisorSum += divisor
+    return divisorSum
+
+def CheckAmicable(a):
+    b = d(a)
+    if d(b) == a and a != b:
+        return 1
+    return 0
+
+amicableSum = 0
+for i in range(1,10000):
+    print(i)
+    if CheckAmicable(i):
+        amicableSum += i
+        
+print(amicableSum)
+# 31626
